5080f51fb68643210b250d95706a47703efa43d8
makipoea <makipoea@gmail.com>
2024-05-19 14:17:23 +0200

    fin de la focntion is_in_polygone

--- graph_analysis.py
+++ graph_analysis.py
@@ -3,13 +3,13 @@ import os
 import logging
 import numpy as np
 import square_generation as oscar 
+import matplotlib.pyplot as plt 
 
-graph = {1:[2, 5], 2:[1, 4], 3:[7], 4:[1, 2, 6, 7], 5:[1, 6], 6:[4, 5], 7:[3, 4], 8:[]}
 
 def backtrack(g, ham_path, l_deja_vu, l_first_sommet_tried):
     """
     implementation pour un dictionnaire d'adjacence d'une recherche d'un chemin hamiltonien
-    ACHTUNG ACHTUNG les liste de voisins doivent trier^tm 
+    ACHTUNG ACHTUNG les liste de voisins doivent etre trier^tm 
     """
     #print(ham_path, l_deja_vu)
     if len(ham_path) >= len(g.keys()): 
@@ -39,22 +39,24 @@ def backtrack(g, ham_path, l_deja_vu, l_first_sommet_tried):
 def is_in_polygone(point_to_compare, polygone):
     """
     take a polygone as a list of point and a point (cartesian coordinates) return boolen
-    /////! !!!! ! ! ! ! ! ! ! \ Cette focntion ne marche pas 
+    utilise la methode de raycasting avec une ligne horizontale
+    # Cette fonction est EXTREMENT LENTE a utiliser avec parsismonie voir mieux a optimiser
     """
     is_inside = 0
     if polygone == []:
         logging.info("is_in_polygone a receive an empty polygone just sayin")
         return False
 
-    last_point = polygone[-1]
-    
-    for point in polygone[1::]+last_point: # cheker si les point sont pas sur un sommet
-        if min(last_point[1], point[1]) <= point_to_compare[1] < max(last_point[1], point[1]): # <= .. < evite theoriquement les cas ou in tombe sur un point
-            if last_point[0] < point[0]:
-                if point_to_compare[1]*(point[0]-last_point[0]) < (point[1]-last_point[1])*(point_to_compare[0] - last_point[0]) + point_to_compare:
-                    is_inside = 1 - is_inside
-            elif point_to_compare[1]*(point[0]-last_point[0]) < (point[1]-last_point[1])*(point_to_compare[0] - last_point[0]) + point_to_compare:
-                is_inside = 1 - is_inside
+    last_point = polygone[0]
+    for point in polygone[1::]+[last_point]:
+        if min(point[1], last_point[1])<=point_to_compare[1] < max(point[1], last_point[1]): # il y a intersection <= ... < pemret de prendre le cas ou le projeter tombe sur un point(compter une seul fois oula je crois que je depasse les 80 characteres) 
+            p = (point_to_compare[1] - last_point[1]) * (point[0] - last_point[0])
+            if point[1] >= last_point[1] and ((point_to_compare[0]-last_point[0])*(point[1]-last_point[1]) <= p): #verifie si le point est avant l'intersection
+                is_inside = 1-is_inside
+
+            elif (point[1] < last_point[1]) and (point_to_compare[0]-last_point[0])*(point[1]-last_point[1]) >= p:
+                is_inside = 1-is_inside
+        last_point = point
 
     if is_inside in [0 , 1]: return is_inside
     else : raise TypeErrorError("la fonction is_in_polygone a lamentablement echouer ")
@@ -64,7 +66,36 @@ def generate_grid(polygone, nb_point):
     """
     retourne une liste de point mais aussi (et surtout) un graph correspondant a une dictionnaire d'adjacence
     """
+    l_point = []
     carre = oscar.square(polygone)
+    #print(carre)
+    eps = (carre[1][0] - carre[0][0])/nb_point
+    for i in range(nb_point):
+        for j in range(nb_point):
+            if is_in_polygone([carre[0][0]+i*eps, carre[0][1]+j*eps], polygone):
+                #print("valide")
+                l_point.append([carre[0][0]+i*eps, carre[0][1]+j*eps])
+    return l_point
 
-print(len(graph.keys()))
-print(backtrack(graph, [], [], []))     
+if __name__ == "__main__":
+
+    polygone = [(1,0),(0.71,0.71),(0,1),(-0.71, 0.71),(-1,0),(-0.71,-0.71),(0,-1),(0.71,-0.71)]
+
+    point = [0.6, 0.9]
+
+    b = is_in_polygone(point, polygone)
+
+    #print(f'resultat = {b}')
+    
+    plt.plot([point[0]for point in polygone], [point[1]for point in polygone])
+    
+    l_grille = generate_grid(polygone, 1000)
+    #print(l_grille)
+    plt.scatter([point[0]for point in l_grille], [point[1]for point in l_grille], s=1)
+
+    print(len(l_grille))
+
+    plt.show()
+    
+    #graph = {1:[2, 5], 2:[1, 4], 3:[7], 4:[1, 2, 6, 7], 5:[1, 6], 6:[4, 5], 7:[3, 4], 8:[]}
+    #print(backtrack(graph, [], [], []))     
